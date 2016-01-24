00439b77f94a76cab6a2c07710e04463d9c36aaa
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-01-25 00:48:09 +0700

    init

--- config/index.js
+++ config/index.js
@@ -1,16 +1,29 @@
-const NODE_ENV = require('../lib/environment');
+import fs from 'fs';
+import _debug from 'debug';
+import config from './_base';
 
-module.exports = exports = {
+const debug = _debug('app:config');
+debug('Create configuration.');
+debug(`Apply environment overrides for NODE_ENV "${config.env}".`);
 
-  // environment
-  __DEBUG__ : NODE_ENV === 'development',
+// Check if the file exists before attempting to require it, this
+// way we can provide better error reporting that overrides
+// weren't applied simply because the file didn't exist.
+const overridesFilename = `_${config.env}`;
+let hasOverridesFile;
+try {
+  fs.lstatSync(`${__dirname}/${overridesFilename}.js`);
+  hasOverridesFile = true;
+} catch (e) {}
 
-  // build system
-  VENDOR_DEPENDENCIES : ['react'],
-  SRC_DIRNAME  : 'app',
-  DIST_DIRNAME : 'dist',
+// Overrides file exists, so we can attempt to require it.
+// We intentionally don't wrap this in a try/catch as we want
+// the Node process to exit if an error occurs.
+let overrides;
+if (hasOverridesFile) {
+  overrides = require(`./${overridesFilename}`)(config);
+} else {
+  debug(`No configuration overrides found for NODE_ENV "${config.env}"`);
+}
 
-  // server
-  WEBPACK_PORT : 2000,
-  SERVER_PORT  : 3000
-};
+export default Object.assign({}, config, overrides);
